bb0ea3f6cdc16c99be1cba7f9e2f356290f189ae
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-12-24 04:16:34 -0800

    multi lang

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const lang of languages) console.log(lang);
