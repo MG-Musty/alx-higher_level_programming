7fb1f0ac64fc6fa41aef99ef7ed1dcad04d70bc1
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-02-11 08:43:03 -0800

    star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+table File  12 lines (10 sloc)  361 Bytes
+
+#!/usr/bin/node
+/* prints the title of a Star Wars movie where the
+episode number matches a given integer */
+
+const request = require('request');
+const id = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+request(apiUrl + id + '/', (err, response, body) => {
+  if (err) console.log(err);
+  else console.log(JSON.parse(body).title);
+});
